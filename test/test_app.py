995ab1345454308cf3f32419ba515e70bba05635
bala151187 <balamurugan151187@gmail.com>
2018-05-24 09:03:47 -0400

    Add files via upload

---
+++ test/test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import hello
+
+class AppTestCase(unittest.TestCase):
+    def test_string(self):     
+        self.assertTrue()
+
+if __name__ == '__main__':
+    unittest.main()
