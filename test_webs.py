83d452b38108a792cb48d3b5141631cd45058d18
bala151187 <balamurugan151187@gmail.com>
2018-05-24 11:20:01 -0400

    Update test_webs.py

--- test_webs.py
+++ test_webs.py
@@ -1,16 +1,18 @@
 import unittest
-import flaskapi
+import webs
 import requests
 import json
 import sys
 
 class TestFlaskApi(unittest.TestCase):
     def setUp(self):
-        self.app = flaskapi.app.test_client()
+        self.app = webs.webserv.test_client()
 
     def test_hello_world(self):
         response = self.app.get('/')
-        self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {'hello': 'world'})
+        #self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {'hello': 'world'})
+        print (response.data)
+        self.assertEqual(response.data.decode('UTF-8'),'Hello World!')
 
 if __name__ == '__main__':
     unittest.main()
