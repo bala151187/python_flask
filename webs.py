1549f607eee1231a6d39b7dbe3f48cfaf00f1e1d
bala151187 <balamurugan151187@gmail.com>
2018-05-24 11:21:26 -0400

    Update webs.py

--- webs.py
+++ webs.py
@@ -1,20 +1,9 @@
 from flask import Flask
-from source.api import app
-from unittest import TestCase
-
 webserv = Flask(__name__)
 
-@app.route("/")
+@webserv.route("/")
 def hello():
     return "Hello World!"
-
-class TestIntegrations(TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_thing(self):
-        response = self.app.get('/')
-        assert "Hello World!"
         
 if __name__ == "__main__":
     webserv.run(host='0.0.0.0',port=8080)
