23b3a635b6a96686988a7f509d73cb55c9e79cc9
bala151187 <balamurugan151187@gmail.com>
2018-05-24 10:00:55 -0400

    Create test_webs.py

---
+++ test_webs.py
@@ -0,0 +1,9 @@
+import unittest
+from webs import hello
+
+class AppTestCase(unittest.TestCase):
+    def test_string(self):     
+        self.assertTrue()
+
+if __name__ == '__main__':
+    unittest.main()
