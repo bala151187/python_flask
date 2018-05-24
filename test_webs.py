f8e9ff3548fcc568ebd2881fb3477e378db839c2
bala151187 <balamurugan151187@gmail.com>
2018-05-24 10:11:27 -0400

    Update test_webs.py

--- test_webs.py
+++ test_webs.py
@@ -3,7 +3,8 @@ from webs import hello
 
 class AppTestCase(unittest.TestCase):
     def test_string(self):     
-        self.assertTrue()
+        rv = self.app.get('/')
+        assert 'Hello World!' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
