8b900d622fb8a846b774a56d75899b214bcb6e3a
bala151187 <balamurugan151187@gmail.com>
2018-05-24 10:14:58 -0400

    Update test_webs.py

--- test_webs.py
+++ test_webs.py
@@ -2,6 +2,9 @@ import unittest
 from webs import hello
 
 class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        
     def test_string(self):     
         rv = self.app.get('/')
         assert 'Hello World!' in rv.data
