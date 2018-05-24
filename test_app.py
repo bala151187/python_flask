c1613ae3d8ad8145f922f8042c1009dc45e6f694
bala151187 <balamurugan151187@gmail.com>
2018-05-24 09:04:03 -0400

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import hello
-
-class AppTestCase(unittest.TestCase):
-    def test_string(self):     
-        self.assertTrue()
-
-if __name__ == '__main__':
-    unittest.main()
